d904361aef83f066ffd905bbece1d330dd223aff
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-12-06 20:12:22 -0600

    OK

--- Histograma.c
+++ Histograma.c
@@ -14,10 +14,9 @@ int *FDA(int *histo_og){
     int *D_Acumulada = malloc(L * sizeof(int));
 
     D_Acumulada[0] = histo_og[0];
-    for(size_t i = 1; i < L; i++){
+    for(int i = 1; i < L; i++){
         D_Acumulada[i] = D_Acumulada[i - 1] + histo_og[i];
     }
-
     return D_Acumulada;
 }
 
@@ -25,42 +24,44 @@ int *histograma_sec(unsigned char *pixeles, int ancho, int alto, int canal){
     int *histograma = malloc(L * sizeof(int));
 
     // parallel for
-    for(size_t i = 0; i < L; i++){
+    for(int i = 0; i < L; i++){
         histograma[i] = 0;
     }
 
-    int i = 0;
-    while (i < ancho*alto*canal){
+    // Iterar sobre todos los píxeles de la imagen
+    for(int i = 0; i < ancho*alto*canal; i += canal){
         histograma[pixeles[i]] += 1;
-        i += canal;
     }
-
     return histograma;
 }
 
-unsigned char *funcion_eq_sec(unsigned char *pixeles, int *f_acu, int ancho, int alto, int canal){
+unsigned char *funcion_eq_sec(unsigned char *pixeles, int *cdf, int ancho, int alto, int canal){
     int *funcion_eq = malloc(L * sizeof(int));
+    int cdf_min;
 
-    int cdf_min = f_acu[0];
+    for(int i = 0; i < L; i++){
+        if(cdf[i] != 0){
+            cdf_min = cdf[i];
+            break;
+        }
+    }    
     int tam = ancho * alto;
 
-    for(size_t i = 0; i < L; i++){
-        int cdf = f_acu[i];
-        funcion_eq[i] = round(((cdf - cdf_min) / (tam - cdf_min)) * 255);
+    for(int i = 0; i < L; i++){
+        funcion_eq[i] = round(((cdf[i] - cdf_min) / ((double)(ancho * alto)) * 254)) + 1;
     }
 
     unsigned char *new_pixeles = malloc(ancho * alto * sizeof(unsigned char));
 
-    for(size_t i = 0; i < ancho*alto; i++){
+    for(int i = 0; i < ancho*alto; i++){
         new_pixeles[i] = funcion_eq[pixeles[i * canal]];
     }
-
     return new_pixeles;
 }
 
 int main(int argc, char *argv[]){
 
-    char *dicIma = "Imagenes_Gris/imaColor1.jpg";
+    char *dicIma = "nature.jpeg";
     int ancho, alto, canales;
     unsigned char *pixeles = stbi_load(dicIma, &ancho, &alto, &canales, 0);
 
@@ -68,15 +69,18 @@ int main(int argc, char *argv[]){
         printf("La imagen no pudo ser cargada correctamente %s", dicIma);
         exit(1);
     }else{
-        printf("Imagen cargada correctamente");
+        printf("\nImagen cargada correctamente\n\n");
     }
 
     int *histo_original = histograma_sec(pixeles,ancho,alto,canales);
     int *f_acumulada = FDA(histo_original);
 
     unsigned char *pixeles_new = funcion_eq_sec(pixeles,f_acumulada,ancho,alto,canales);
+    int *histo_modificado = histograma_sec(pixeles_new,ancho,alto,1);
 
-    stbi_write_jpg("New.jpg", ancho, alto, 3, pixeles_new, 90);
+    stbi_write_jpg("New_1.jpg", ancho, alto, 1, pixeles_new, 100);
     stbi_image_free(pixeles_new);
+
+    
 }
 
