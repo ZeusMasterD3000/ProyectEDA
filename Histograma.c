8bda425131d41ef403dc8a8982538eca1c59c396
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-12-06 21:11:08 -0600

    Secuencial

--- Histograma.c
+++ Histograma.c
@@ -59,14 +59,46 @@ unsigned char *funcion_eq_sec(unsigned char *pixeles, int *cdf, int ancho, int a
     return new_pixeles;
 }
 
+char *cambiar_name(const char *name, int op) {
+    char *name_aux = NULL;
+    char *extension = ".jpg";
+    char *punto = strrchr(name, '.');
+    
+    if (punto != NULL && strcmp(punto, extension) == 0) {
+        if (op == 1) {
+            asprintf(&name_aux, "%.*s_eq_secuencial.jpg", (int)(punto - name), name);
+        } else if (op == 2) {
+            asprintf(&name_aux, "%.*s_eq_paralelo.jpg", (int)(punto - name), name);
+        } else if (op == 3) {
+            asprintf(&name_aux, "%.*s_histo_secuencial.csv", (int)(punto - name), name);
+        } else if (op == 4) {
+            asprintf(&name_aux, "%.*s_histo_paralelo.csv", (int)(punto - name), name);
+        }
+    }
+    return name_aux;
+}
+
+void archivoCSV(int *histograma_og, int *histograma_eq, char *name, int op) {
+    char *name_new = cambiar_name(name,op);
+
+    FILE *csv = fopen(name_new, "w");
+
+    for (int i = 0; i < L; i++) {
+        fprintf(csv, "%d,%d,%d\n", i, histograma_og[i], histograma_eq[i]);
+    }
+    fclose(csv);
+    printf("Datos escritos correctamente en %s\n", name_new);
+}
+
 int main(int argc, char *argv[]){
 
-    char *dicIma = "nature.jpeg";
+    char *dicIma = argv[1];
     int ancho, alto, canales;
     unsigned char *pixeles = stbi_load(dicIma, &ancho, &alto, &canales, 0);
 
     if(pixeles == NULL){
-        printf("La imagen no pudo ser cargada correctamente %s", dicIma);
+        printf("\nLa imagen no pudo ser cargada correctamente %s", dicIma);
+        printf("\nVerificar que sea '.jpg' o que haya puesto bien la dirección\n");
         exit(1);
     }else{
         printf("\nImagen cargada correctamente\n\n");
@@ -78,9 +110,11 @@ int main(int argc, char *argv[]){
     unsigned char *pixeles_new = funcion_eq_sec(pixeles,f_acumulada,ancho,alto,canales);
     int *histo_modificado = histograma_sec(pixeles_new,ancho,alto,1);
 
-    stbi_write_jpg("New_1.jpg", ancho, alto, 1, pixeles_new, 100);
+    stbi_write_jpg(cambiar_name(dicIma,1), ancho, alto, 1, pixeles_new, 100);
     stbi_image_free(pixeles_new);
 
-    
+    archivoCSV(histo_original,histo_modificado,dicIma,3);
+
+    return 0;
 }
 
