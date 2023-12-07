ef5ed4832efcabb3ed2092a6ac02538d1d834372
ZeusMasterD3000 <snowfallk391@gmail.com>
2023-12-06 18:04:33 -0600

    Casi

--- Histograma.c
+++ Histograma.c
@@ -1,55 +1,82 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include<math.h>
 #include <omp.h>
 
-#define L 10 //256
-#define prints 1
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb-master/stb_image.h"
 
-int *Histograma_sec(int **imagen, int ren, int col){
-    int histograma[L];
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb-master/stb_image_write.h"
+
+#define L 256
+
+int *FDA(int *histo_og){
+    int *D_Acumulada = malloc(L * sizeof(int));
+
+    D_Acumulada[0] = histo_og[0];
+    for(size_t i = 1; i < L; i++){
+        D_Acumulada[i] = D_Acumulada[i - 1] + histo_og[i];
+    }
+
+    return D_Acumulada;
+}
+
+int *histograma_sec(unsigned char *pixeles, int ancho, int alto, int canal){
+    int *histograma = malloc(L * sizeof(int));
+
+    // parallel for
     for(size_t i = 0; i < L; i++){
         histograma[i] = 0;
     }
-    for(size_t i = 0; i < ren; i++){
-        for(size_t j = 0; j < col; j++){
-            histograma[imagen[i][j]] += 1;
-        }
+
+    int i = 0;
+    while (i < ancho*alto*canal){
+        histograma[pixeles[i]] += 1;
+        i += canal;
     }
-    for(size_t i = 0; i < L; i++){
-        printf("%d: %d\n",i, histograma[i]);
-    }
+
     return histograma;
 }
-int **RandomImage(int ren, int col){
-    // Imagen
-    int **imagen = malloc(ren * sizeof(int *));
-    
-    for(size_t i = 0; i < ren; i++){
-        imagen[i] = malloc(col * sizeof(int));
+
+unsigned char *funcion_eq_sec(unsigned char *pixeles, int *f_acu, int ancho, int alto, int canal){
+    int *funcion_eq = malloc(L * sizeof(int));
+
+    int cdf_min = f_acu[0];
+    int tam = ancho * alto;
+
+    for(size_t i = 0; i < L; i++){
+        int cdf = f_acu[i];
+        funcion_eq[i] = round(((cdf - cdf_min) / (tam - cdf_min)) * 255);
     }
-    for(size_t i = 0; i < ren; i++){
-        for(size_t j = 0; j < col; j++){
-            imagen[i][j] = rand() % L;
-        }
+
+    unsigned char *new_pixeles = malloc(ancho * alto * sizeof(unsigned char));
+
+    for(size_t i = 0; i < ancho*alto; i++){
+        new_pixeles[i] = funcion_eq[pixeles[i * canal]];
     }
-    if(prints != 0){
-        for(size_t i = 0; i < ren; i++){
-            for(size_t j = 0; j < col; j++){
-                printf("%d\t",imagen[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-    return imagen;
+
+    return new_pixeles;
 }
 
-int main(){
-    int ren = 5;
-    int col = 5;
+int main(int argc, char *argv[]){
 
-    int **ima = RandomImage(ren,col);
-    int *his = Histograma_sec(ima,ren,col);
-    //TODO: HAcer free
+    char *dicIma = "Imagenes_Gris/imaColor1.jpg";
+    int ancho, alto, canales;
+    unsigned char *pixeles = stbi_load(dicIma, &ancho, &alto, &canales, 0);
+
+    if(pixeles == NULL){
+        printf("La imagen no pudo ser cargada correctamente %s", dicIma);
+        exit(1);
+    }else{
+        printf("Imagen cargada correctamente");
+    }
+
+    int *histo_original = histograma_sec(pixeles,ancho,alto,canales);
+    int *f_acumulada = FDA(histo_original);
+
+    unsigned char *pixeles_new = funcion_eq_sec(pixeles,f_acumulada,ancho,alto,canales);
+
+    stbi_write_jpg("New.jpg", ancho, alto, 3, pixeles_new, 90);
+    stbi_image_free(pixeles_new);
 }
-// Luego le avanzo
+
